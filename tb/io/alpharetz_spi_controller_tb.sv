767e92e2b2ae376522126572f06cb5262c380241
vani2620 <3idiotsrule@gmail.com>
2025-01-29 20:18:33 -0500

    Renamed directories Replaced inverted sync reset with normal sync reset

--- tb/io/alpharetz_spi_controller_tb.sv
+++ tb/io/alpharetz_spi_controller_tb.sv
@@ -3,7 +3,7 @@
  *Date created: 09012025
  */
 
-`include "../../src/spi/alpharetz_spi_params.svh"
+`include "alpharetz_spi_params.svh"
 `default_nettype none
 // verilog_lint: waive-start line-length
 // verilog_lint: waive-start parameter-name-style
@@ -12,7 +12,7 @@ module alpharetz_spi_controller_tb;
 
 reg sys_clk_tb;
 reg sys_clk_en_tb;
-reg sync_rst_n_tb;
+reg sync_rst_tb;
 reg start_txn_tb;
 reg [SPI_DATA_WIDTH-1:0] tx_data_tb;
 reg [P_ADDR_WIDTH-1:0] p_addr_tb;
@@ -26,7 +26,7 @@ reg [15:0] clk_counter_tb;
 alpharetz_spi_controller spi_ctrl_tb(
     .sys_clk(sys_clk_tb),
     .sys_clk_en(sys_clk_en_tb),
-    .sync_rst_n(sync_rst_n_tb),
+    .sync_rst(sync_rst_tb),
     .start_txn(start_txn_tb),
     .tx_data(tx_data_tb),
     .p_addr(p_addr_tb),
@@ -48,9 +48,9 @@ end
 
 initial begin
     sys_clk_en_tb = 1;
-    sync_rst_n_tb = 1;
-    #10 sync_rst_n_tb = 0;
-    #10 sync_rst_n_tb = 1;
+    sync_rst_tb = 0;
+    #10 sync_rst_tb = 1;
+    #10 sync_rst_tb = 0;
 end
 
 endmodule
--- tb/parts/clock_divider_tb.sv
+++ tb/parts/clock_divider_tb.sv
@@ -3,7 +3,7 @@
  *Date created: 08012025
  */
 
-`include "../../src/spi/spi_params.svh"
+`include "alpharetz_spi_params.svh"
 `default_nettype none
 `timescale 1ns/1ps
 
@@ -16,7 +16,7 @@ module clock_divider_tb;
 
 reg clk_in_tb;
 reg clk_en_tb;
-reg sync_rst_n_tb;
+reg sync_rst_tb;
 reg cpol;
 wire [15:0] counter_out_tb;
 wire clk_out_tb;
@@ -26,7 +26,7 @@ clock_divider #(
 ) clk_div_tb (
     .clk_in(clk_in_tb),
     .clk_en(clk_en_tb),
-    .sync_rst_n(sync_rst_n_tb),
+    .sync_rst(sync_rst_tb),
     .cpol(cpol),
 
     .counter_out(counter_out_tb),
@@ -42,14 +42,14 @@ end
 
 initial begin
     clk_en_tb = 1;
-    sync_rst_n_tb = 1;
-    #10 sync_rst_n_tb = 0;
-    #10 sync_rst_n_tb = 1;
+    sync_rst_tb = 0;
+    #10 sync_rst_tb = 1;
+    #10 sync_rst_tb = 0;
 end
 
 initial begin
     $monitor("time = %3d, clk_in = %b, reset = %b, counter = %5d, clk_out = %b",
-            $time, clk_in_tb, sync_rst_n_tb, counter_out_tb, clk_out_tb);
+            $time, clk_in_tb, sync_rst_tb, counter_out_tb, clk_out_tb);
 end
 
 endmodule
